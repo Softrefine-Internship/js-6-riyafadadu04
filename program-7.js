60d0c23bbd75eda7cac7e3391b8d5ca3e7910926
riyafadadu04 <riyafadadu2004@gmail.com>
2024-10-03 16:15:56 +0530

    Program-7 Completed

--- program-7.js
+++ program-7.js
@@ -1,1 +1,11 @@
 // Write a JavaScript program to implement a function that executes a given function repeatedly at a fixed interval using 'setInterval()'.
+
+function callingFunction (){
+    console.log('Function 1 executed');
+}
+
+function repeatFunction(func, interval){
+    setInterval(func, interval);
+}
+
+repeatFunction(callingFunction, 2000); 
