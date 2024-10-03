0252e335f90afb55f0e6cdbeee7543d24e5f56a3
riyafadadu04 <riyafadadu2004@gmail.com>
2024-10-03 14:48:13 +0530

    Program-1 Completed

--- program-1.js
+++ program-1.js
@@ -1,1 +1,12 @@
 // Write a JavaScript function that takes a callback and invokes it after a delay of 2 second.
+
+function delayAndCallback(callback) {
+    setTimeout(callback, 2000);
+}
+
+function callback(){
+    console.log('called after 2 seconds');
+}
+
+delayAndCallback(callback); 
+console.log('callback after 2 seconds');
