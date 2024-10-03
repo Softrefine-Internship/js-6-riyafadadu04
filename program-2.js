9e186a0f8636be9bd0b1865dc5a2f3d86f9494c0
riyafadadu04 <riyafadadu2004@gmail.com>
2024-10-03 15:06:37 +0530

    Program-2 Completed

--- program-2.js
+++ program-2.js
@@ -1,1 +1,24 @@
 // Write a JavaScript a function that makes an HTTP GET request and returns a Promise that resolves with the response data.
+
+
+const url =
+  "https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e867b";
+
+function fetchData(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Not found (${response.status})`);
+        }
+        return resolve(response.json());
+      })
+      .catch((err) => reject(err));
+  });
+}
+
+fetchData(url)
+  .then((response) =>
+    console.log("Recipe Name :" , response.data.recipe.title)
+  )
+  .catch((error) => console.error("Error:", error));
