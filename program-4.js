ab3e7b918acb869108aaf062fbc1234552498db6
riyafadadu04 <riyafadadu2004@gmail.com>
2024-10-03 15:43:38 +0530

    Program-4 Completed

--- program-4.js
+++ program-4.js
@@ -1,3 +1,28 @@
 // Write a JavaScript program that implements a function that performs a series of asynchronous operations in sequence using 'async/await'.
 
 // note: here async operations refers to async functions. your task is to write multiple async functions using promises and call them in sequence in a function using async/await
+
+const function1 = async function () {
+  const func11 = await Promise.resolve("Function 1's 1st promise completed");
+  console.log(func11);
+  const func12 = await Promise.resolve("Function 1's 2nd promise completed");
+  console.log(func12);
+};
+
+const function2 = async function () {
+  const func2 = await Promise.resolve("Function 2 completed");
+  console.log(func2);
+};
+
+const function3 = async function () {
+  const func3 = await Promise.resolve("Function 3 completed");
+  console.log(func3);
+};
+
+const sequence = async function () {
+  await function1();
+  await function2();
+  await function3();
+};
+
+sequence();
